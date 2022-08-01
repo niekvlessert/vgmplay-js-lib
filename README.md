8aece8021e58c52f72d0268f6058ba86231c81d4
Niek Vlessert <niekvlessert@gmail.com>
2022-08-01 22:05:05 +0200

    Compiles now

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # vgmplay-js-lib
+
+Project to get to emscripten transpiled libvgm based library for playing VGM files, using an audioworklet.
+
+Currently it's based on vgmplay and using the old scriptprocessor. Other versions will be in other branches. Building should work with actions, result should be on github.io
+
+The index.html includes a very bary demo, it just plays.
+
+Run it on a https webserver (thisisunsafe for Chrome errors on a local ssl webserver, please google...).
+
+- Visit the page: https://ip/vgmplay-js-lib/
+- Press load
+- Press play
+
+A music file is included in the data file which is generated in the compilation process.
+
+Building:
+
+```
+cd /var/www/html/
+git clone --recursive https://github.com/niekvlessert/vgmplay-js-lib.git
+cd vgmplay-js-lib
+mkdir build
+cd build
+emcmake cmake ..
+make
+cp *.data ..
+```
---
+++ src/main.c
@@ -0,0 +1,80 @@
+#include "../vgmplay/VGMPlay/chips/mamedef.h"
+#include <wchar.h>
+#include "../vgmplay/VGMPlay/stdbool.h"
+#include "../vgmplay/VGMPlay/VGMPlay.h"
+#include "../vgmplay/VGMPlay/VGMPlay_Intf.h"
+#include <emscripten.h>
+#include <zlib.h>
+#include <locale.h>
+
+
+extern UINT32 SampleRate;
+extern bool VGMEnd;
+extern VGM_HEADER VGMHead;
+extern UINT32 VGMMaxLoop;
+extern GD3_TAG VGMTag;
+
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "");
+	EM_ASM(
+		moduleInitialized=true;
+	);
+	return 0;
+}
+
+void FillBuffer2(INT16* Left, INT16* Right){
+        WAVE_16BS sample_buffer[ 16384 ];
+        FillBuffer(sample_buffer, 16384);
+        for (int i = 0; i<16384; i++ ){
+                Left[i]=sample_buffer[i].Left;
+                Right[i]=sample_buffer[i].Right;
+        }
+}
+
+void SetSampleRate(UINT32 _SampleRate){
+	SampleRate = _SampleRate;
+}
+
+void SetLoopCount(UINT32 _LoopCount){
+	VGMMaxLoop = _LoopCount;
+}
+
+int VGMEnded(void) {
+	if (VGMEnd) return 1; else return 0;
+}
+
+int GetFileLength(VGM_HEADER* FileHead)
+{
+        UINT32 SmplCnt;
+        // Note: SmplCnt is ALWAYS 44.1 KHz, VGM's native sample rate
+        SmplCnt = FileHead->lngTotalSamples + FileHead->lngLoopSamples * (VGMMaxLoop - 0x01);
+
+        return SmplCnt;
+}
+
+int GetTrackLength(void) {
+        return SampleVGM2Playback(GetFileLength(&VGMHead));
+}
+
+int GetLoopPoint(void) {
+        return SampleVGM2Playback(VGMHead.lngLoopSamples);
+}
+
+char * ShowTitle(void) {
+	//VGMTag is wchar_t, use sprintf to get it to char. UTF-8 is max 4 bytes per char. Assume that with malloc.
+	char *buffer = malloc((
+		wcslen(VGMTag.strTrackNameE)
+		+wcslen(VGMTag.strTrackNameJ)
+		+wcslen(VGMTag.strGameNameE)
+		+wcslen(VGMTag.strGameNameJ)
+		+wcslen(VGMTag.strSystemNameE)
+		+wcslen(VGMTag.strSystemNameJ)
+		+wcslen(VGMTag.strAuthorNameE)
+		+wcslen(VGMTag.strAuthorNameJ)
+		+wcslen(VGMTag.strCreator)
+		+wcslen(VGMTag.strNotes)
+		+27+1)*4); // delimters + \0 
+	sprintf(buffer,"%ls|||%ls|||%ls|||%ls|||%ls|||%ls|||%ls|||%ls|||%ls|||%ls\n", VGMTag.strTrackNameE,VGMTag.strTrackNameJ,VGMTag.strGameNameE,VGMTag.strGameNameJ,VGMTag.strSystemNameE,VGMTag.strSystemNameJ,VGMTag.strAuthorNameE,VGMTag.strAuthorNameJ,VGMTag.strCreator,VGMTag.strNotes);
+	return buffer;
+}
